fcfef1c46a99a8c1249529f640264df6a453cb43
Mayerly Andrea Monroy Malagón <131180877+Mayerly424@users.noreply.github.com>
2024-03-09 09:36:14 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <header>
+
+                <div className="container">
+                    <nav className="nav">
+                        <div className="menu-toggle">
+                            <i className="fas fa-bars" />
+                            <i className="fas fa-times" />
+                        </div>
+                        <a href="Index.html" className="logo">JOYASKOGI</a>
+                        <ul className="nav-list">
+                            <li className="nav-item">
+                                <a href="Index.html" className="nav-link active">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="Colecciones.html" className="nav-link ">Colecciones</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="Galeria.html" className="nav-link ">Galeria</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="Cuidado.html" className="nav-link ">Cuidado</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="Nosotros.html" className="nav-link ">Nosotros</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="Sesión.html" className="nav-link ">Iniciar sesión</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="Contactanos.html" className="nav-link ">Contactanos</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <section className="hero" id="hero">
+                    <div className="container">
+                        <h2 className="h2-sub">
+                            <span className="fil">B</span>ienvenido
+                        </h2>
+                        <h1 className="head">Joyeria Kogi</h1>
+                        <div className="he-des">
+                            <h5>Expertos artesanos en diseño, desarrollo y produccion de accesorios de joyeria</h5>
+                            <a href="Nosotros.html" className="btn cta-btn">Explora</a>
+                        </div>
+                    </div>
+                </section>
+
+
+            </header>
+        </div>
+
+    );
+}
+
+export default Header;
